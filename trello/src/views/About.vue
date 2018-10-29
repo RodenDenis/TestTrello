3c0c628943f4e2a57435d7e385df3bde32edcee3
Roden Denis <kievdenisromanov@gmail.com>
2018-10-29 13:46:32 +0200

    Переписал весь код
    По отдельности работает создание новых тикетов, их редактирование через модал и отображение на странице в тикете.
    При попытке собрать вместе создание списков и тикетов получилась ошибка: при первой загрузке не отображается название списка; нет кнопки создания новых тикетов в списке.

--- trello/src/views/About.vue
+++ trello/src/views/About.vue
@@ -1,117 +1,42 @@
 <template>
-    <TaskList />
+  <div>
+    <create-list v-on:create-list="createList"></create-list>
+    <todo-list v-bind:list="list"></todo-list>
+    
+  </div>
 </template>
 
 <script>
-import TaskList from "@/components/tasklist.vue"
-
+import Todo from "@/components/Todo.vue";
+import TodoList from "@/components/TodoList.vue";
+import CreateTodo from "@/components/CreateTodo.vue";
+import CreateList from "@/components/CreateList.vue";
 export default {
+  name: "app",
   components: {
-    TaskList
+    Todo,
+    TodoList,
+    CreateTodo,
+    CreateList
   },
   data() {
     return {
-      tasks: [
-        {title: ""}
-       ]
-    }
-  }
-}
-
+      list: [{
+        title: "Edit list title"
+      }],
+      todo: [{
+        title: "Edit todo",
+        task: "Edit todo",
+      }],
+    };
+  },
+  methods: {
+    createTodo(newTodo) {
+      this.todo.push(newTodo);
+    },
+    createList(newList) {
+      this.list.push(newList);
+    },
+  },
+};
 </script>
-<style>
-body {
-  background-color: #026aa7;
-}
-
-.tasks {
-  width: 100%;
-  overflow: auto;
-  background-color:  rgb(0, 121, 191);
-}
-.tasks-board {
-margin-left: -40px;
-}
-.tasks-list {
-  width: 272px;
-  list-style-type: none;
-  margin-bottom: 5px;
-  background-color: #dfe3e6;
-  min-height: 60px;
-  border-radius: 5px;
-  margin-right: 20px;
-  display: block;
-  position: relative;
-  float: left;
-}
-.tasks-list-decore {
- list-style-type: none;
- padding: 0;
-}
-
-ul.tasks-list {
-  padding: 0;
-}
-.tasks-list-title {
-  height: 24px;
-  cursor: pointer;
-  width: 100%;
-  text-align: left;
-  border: none;
-  outline: none;
-  font-size: 20px;
-  border-radius: 5px 5px 0 0;
-  background-color: #dfe3e6;
-}
-.tasks-list-title:hover {
-  border-color: rgba(0,0,0,0.15);
-}
-.tasks-footer-add {
-  height: 24px;
-  cursor: pointer;
-  width: 100%;
-  text-align: left;
-  border: none;
-  outline: none;
-  font-size: 14px;
-  border-radius: 0 0 5px 5px;
-  background-color: #dfe3e6;
-}
-.tasks-footer-add:hover {
-  text-decoration: underline;
-  background-color: LightGray;
-}
-.tasks-item-text {
-  min-height: 60px;
-  max-height: 140px;
-  width: 94%;
-  background-color: white;
-  margin-left: auto;
-  margin-right: auto;
-  border-radius: 5px;
-  padding-left: 5px;
-  border: none;
-  resize: none;
-  outline: none;
- 
-}
-.tasks-item-text:hover {
-  cursor: pointer;
-}
-.hide {
-  display: none;
-}
-.tasks-new {
-  margin-bottom: 10px;
-}
-.tasks-item-text-edit {
-  background-color: transparent;
-  border: none;
-  cursor: pointer;
-  position: absolute;
-  transform: translateX(-35px);
-  border-top-right-radius: 5px;
-  outline: none;
-}
-
-</style>
